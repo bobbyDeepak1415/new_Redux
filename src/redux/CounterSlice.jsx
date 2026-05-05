13c2bb80a7d3e6988b637a9cc520d2f597122549
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-06 02:55:35 +0530

    setup done

---
+++ src/redux/CounterSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState={
+    count:0
+}
+
+
+const CounterSlice=createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        
+    }
+})
